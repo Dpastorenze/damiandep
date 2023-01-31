48e102bfbad1ee2e173fe44d4f2084d036457199
damian <dampc1991@gmail.com>
2023-01-31 11:22:46 -0300

    primer proyecto

---
+++ Java/contacto.js
@@ -0,0 +1,16 @@
+function Contacto() {
+    console.log("Formulario de contacto");
+    document.getElementById('formcontacto').style.display='block';
+
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#mailc')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `mailto:dampc1991@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+}
+}
---
+++ Java/personal.js
@@ -0,0 +1,10 @@
+function mostrar() {
+    console.log("Muestra informacion personal");
+    document.getElementById('personalid').style.display='block';
+
+}
+function ocultar() {
+    console.log("Oculta informacion personal");
+    document.getElementById('personalid').style.display='none';
+
+}

eff8d44f19a316a74ad1ae366b0c8143092522e2
Prophet91 <dampc1991@gmail.com>
2023-02-01 08:54:31 -0300

    Update contacto.js

--- Java/contacto.js
+++ Java/contacto.js
@@ -10,7 +10,7 @@ $form.addEventListener('submit', handleSubmit)
 function handleSubmit(event) {
   event.preventDefault()
   const form = new FormData(this)
-  $buttonMailto.setAttribute('href', `mailto:dampc1991@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.setAttribute('href', `mailto:dampc1991@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('asunto')}${form.get('message')} telefono de contacto ${form.get('telefono')}`)
   $buttonMailto.click()
 }
 }
